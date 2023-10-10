f794817656a44a60f9d86c9c7a6fe31ebb458ff1
Ram kumar <ramoffical06@gmail.com>
2023-10-10 20:23:09 +0530

    to-do list

---
+++ todolist/script.js
@@ -0,0 +1,24 @@
+const todoList =document.getElementById("todolist");
+const newTodoInput = document.getElementById("newtodoInput");
+const addTodoBtn =document.getElementById("addtodoBtn"); 
+
+addTodoBtn.addEventListener("click",()=>{
+
+const newTodoText = newTodoInput.value;
+
+if(newTodoText !== ""){
+    const newTodoItem = document.createElement("li");
+    newTodoItem.innerText = newTodoText;
+    const deleteTodoBtn = document.createElement("button");
+    deleteTodoBtn.innerText ="X"; 
+
+    deleteTodoBtn.classList.add("delete-todo-btn");
+    deleteTodoBtn.addEventListener("click",function(){
+        newTodoItem.remove();
+    });
+
+    newTodoItem.appendChild(deleteTodoBtn);
+    todoList.appendChild(newTodoItem);
+    newTodoInput.value="";
+}
+});
